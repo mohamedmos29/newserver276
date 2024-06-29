772712a11bb5c33ea03e3fa75c4d12a97bd230b0
Habiba <93494862+Habeeba00@users.noreply.github.com>
2024-06-30 02:32:28 +0300

    Update viewsets.py

--- erd/viewsets.py
+++ erd/viewsets.py
@@ -1,18 +1,18 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from .Serializers import UserSerializer
 from rest_framework import viewsets
-from .models import Profile
+# from .models import Profile
 from .permissions import IsUserOwnerOrGetAndPostOnly
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-from django.contrib.auth.models import User
-from .models import Register, Profile
+# from django.contrib.auth.models import User
+from .models import Register
 from .Serializers import RegisterSerializers
 
-class UserViewSet(viewsets.ModelViewSet):
-    permission_classes=[IsUserOwnerOrGetAndPostOnly]
-    queryset=User.objects.all()
-    serializer_class=UserSerializer
+# class UserViewSet(viewsets.ModelViewSet):
+#     permission_classes=[IsUserOwnerOrGetAndPostOnly]
+#     queryset=User.objects.all()
+#     serializer_class=UserSerializer
 
 
 
