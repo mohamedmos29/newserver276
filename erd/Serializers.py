c94e74b1dc6544dcb81a39e079c2b26a28d0e455
Habiba <93494862+Habeeba00@users.noreply.github.com>
2024-06-30 02:01:13 +0300

    Update Serializers.py

--- erd/Serializers.py
+++ erd/Serializers.py
@@ -4,54 +4,21 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 
-# class UserSerializer(serializers.ModelSerializer):
-#     password=serializers.CharField(write_only=True,required=False)
-#     username= serializers.CharField(read_only=True)
-
-
-#     def create(self, validated_data):
-#         password=validated_data.pop('password')
-#         user =User.objects.create(**validated_data)
-#         user.save()
-
-#         return user
-
-#     class Meta:
-#         model=User
-#         fields=['url','id','username','email','first_name','last_name','password']
-
-class ProfileSerializer(serializers.ModelSerializer):
-    username = serializers.ReadOnlyField(source='user.username')
-
-    class Meta:
-        model = Profile
-        fields = ['user', 'image', 'username']
-
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=False)
-    profile = ProfileSerializer(read_only=True)  # Include profile data
+    password=serializers.CharField(write_only=True,required=False)
+    username= serializers.CharField(read_only=True)
+
 
     def create(self, validated_data):
-        password = validated_data.pop('password', None)
-        user = User(**validated_data)
-        if password:
-            user.set_password(password)
+        password=validated_data.pop('password')
+        user =User.objects.create(**validated_data)
         user.save()
+
         return user
 
-    def update(self, instance, validated_data):
-        password = validated_data.pop('password', None)
-        for attr, value in validated_data.items():
-            setattr(instance, attr, value)
-        if password:
-            instance.set_password(password)
-        instance.save()
-        return instance
-
     class Meta:
-        model = User
-        fields = ['url', 'id', 'username', 'email', 'first_name', 'last_name', 'password', 'profile']
-
+        model=User
+        fields=['url','id','username','email','first_name','last_name','password']
 
 
 
