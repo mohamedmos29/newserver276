601166345c033f75005031206b71adc3005bb299
Habiba <93494862+Habeeba00@users.noreply.github.com>
2024-06-30 02:30:39 +0300

    Update urls.py

--- erd/urls.py
+++ erd/urls.py
@@ -15,14 +15,14 @@ api_url_patterns=[
 
 router = DefaultRouter()
 router.register(r'patient', views.GetPatient,basename='patient')
-router.register(r'Profile', views.UserSerializer,basename='Profile')
+# router.register(r'Profile', views.UserSerializer,basename='Profile')
 router.register(r'Escort', views.GetEscort,basename='Escort')
 router.register(r'Reminder', views.GetReminder,basename='Reminder')
 router.register(r'Diseases', views.GetDiseases,basename='Diseases')
 router.register(r'Document', views.GetDocument,basename='Document')
 router.register(r'Medicine', views.GetMedicine,basename='Medicine')
 router.register(r'Register', views.GetRegister,basename='Register')
-# router.register(r'Login', views.GetLogin,basename='Login')
+router.register(r'Login', views.getLogin,basename='Login')
 
 
 urlpatterns = [
