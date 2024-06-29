9fc6c211ebebae5cbf3fe438c89e6e6d22450769
Habiba <93494862+Habeeba00@users.noreply.github.com>
2024-06-30 02:15:12 +0300

    Update Serializers.py

--- erd/Serializers.py
+++ erd/Serializers.py
@@ -1,24 +1,17 @@
 from rest_framework import serializers
-from .models import patient,Escort,Medicine,Diseases,Document,Reminder,Register
-from django.contrib.auth.models import User
-from .models import Profile
+from .models import patient,Escort,Medicine,Diseases,Document,Reminder,Register,Login
+# from django.contrib.auth.models import User
+# from .models import Profile
 
 
-class UserSerializer(serializers.ModelSerializer):
+class LoginSerializer(serializers.ModelSerializer):
     password=serializers.CharField(write_only=True,required=False)
     username= serializers.CharField(read_only=True)
 
 
-    def create(self, validated_data):
-        password=validated_data.pop('password')
-        user =User.objects.create(**validated_data)
-        user.save()
-
-        return user
-
     class Meta:
-        model=User
-        fields=['url','id','username','email','first_name','last_name','password']
+        model=Login
+        fields=['username','password']
 
 
 
