59ddd5b045940a37b180c34afda0004cbf846623
Habiba <93494862+Habeeba00@users.noreply.github.com>
2024-06-30 03:25:11 +0300

    Update viewsets.py

--- erd/viewsets.py
+++ erd/viewsets.py
@@ -1,18 +1,14 @@
-# from django.contrib.auth.models import User
-# from .Serializers import UserSerializer
 from rest_framework import viewsets
-# from .models import Profile
 from .permissions import IsUserOwnerOrGetAndPostOnly
 from rest_framework import viewsets, status
 from rest_framework.response import Response
-# from django.contrib.auth.models import User
-from .models import Register
-from .Serializers import RegisterSerializers
+from .models import Register,Login
+from .Serializers import RegisterSerializers,LoginSerializer
 
-# class UserViewSet(viewsets.ModelViewSet):
-#     permission_classes=[IsUserOwnerOrGetAndPostOnly]
-#     queryset=User.objects.all()
-#     serializer_class=UserSerializer
+class UserViewSet(viewsets.ModelViewSet):
+    permission_classes=[IsUserOwnerOrGetAndPostOnly]
+    queryset=Login.objects.all()
+    serializer_class=LoginSerializer
 
 
 
@@ -27,14 +23,14 @@ class RegisterViewSet(viewsets.ModelViewSet):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
             # Create a User instance
-            user = User.objects.create_user(
+            user = Login.objects.create_user(
                 username=serializer.validated_data['name'],
                 email=serializer.validated_data['emaill'],
                 password=serializer.validated_data['Password']
             )
 
             # Create a Profile instance linked to the User
-            profile = Profile.objects.create(
+            profile = Login.objects.create(
                 user=user
                 # You can handle 'image' field from serializer if needed
             )
@@ -42,4 +38,5 @@ class RegisterViewSet(viewsets.ModelViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
